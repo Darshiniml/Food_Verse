38d418609c5d66a082f118fc42960f346f7c9ea9
Darshini ml <mldarshini933@gmail.com>
2026-07-13 21:51:04 +0530

    Remove database secret fallback

--- src/main/java/com/food_application/utility/DBConnection.java
+++ src/main/java/com/food_application/utility/DBConnection.java
@@ -15,18 +15,18 @@ public class DBConnection {
             System.getProperty("foodverse.db.url",
                     System.getenv().getOrDefault(
                             "FOODVERSE_DB_URL",
-                            "jdbc:mysql://localhost:3306/food_app"));
+                            ""));
 
     private static final String USERNAME =
             System.getProperty("foodverse.db.username",
                     System.getenv().getOrDefault(
                             "FOODVERSE_DB_USERNAME",
-                            "root"));
+                            ""));
     private static final String PASSWORD =
             System.getProperty("foodverse.db.password",
                     System.getenv().getOrDefault(
                             "FOODVERSE_DB_PASSWORD",
-                            "Darsh123"));
+                            ""));
     private static final ThreadLocal<Connection> BOUND_CONNECTION =
             new ThreadLocal<>();
 
